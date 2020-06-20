45b6a3bcefcdfbddb45fc37f20abb5f60eadca32
WangKevin <kevin22413180@gmail.com>
2020-06-20 15:46:05 +0800

    Update Resume.js

--- js/Resume.js
+++ js/Resume.js
@@ -1,31 +1,52 @@
-//$("body").parent().css("overflow-y","hidden");
+var div0position = $('#div0').position().top - 50 + 'px';
+var div1position = $('#div1').position().top - 50 + 'px';
+var div2position = $('#div2').position().top - 50 + 'px';
+var div3position = $('#div3').position().top - 50 + 'px';
+
 
 $("#content1").click(function(){
-  //alert("Hello! I am an alert box!");
-  $("#test1").fadeIn();
-  $("#test2").css("display","none");
-  $("#test3").css("display","none");
-  $("#test4").css("display","none");
+  $("body, html").animate({
+    scrollTop: div0position
+  }, 500)
+  $("#div0").animate({
+    scrollTop: div1position
+  }, 500)
 });
 
 $("#content2").click(function(){
-  $("#test2").fadeIn();
-  $("#test1").css("display","none");
-  $("#test3").css("display","none");
-  $("#test4").css("display","none");
-});
+  $("body, html").animate({
+    scrollTop: div0position
+  }, 500)
+  $("#div0").animate({
+    scrollTop: div2position
+  }, 500)
+})
 
 $("#content3").click(function(){
-  $("#test3").fadeIn();
-  $("#test1").css("display","none");
-  $("#test2").css("display","none");
-  $("#test4").css("display","none");
+  $("body, html").animate({
+    scrollTop: div0position
+  }, 500)
+  $("#div0").animate({
+    scrollTop: div3position
+  }, 500)
 });
 
-$("#content4").click(function(){
-  $("#test4").fadeIn();
-  $("#test1").css("display","none");
-  $("#test2").css("display","none");
-  $("#test3").css("display","none");
-});
 
+$("#content1").mousedown(function () { 
+  $("#content1").addClass("text-white");
+});
+$("#content1").mouseup(function () { 
+  $("#content1").removeClass("text-white");
+});
+$("#content2").mousedown(function () { 
+  $("#content2").addClass("text-white");
+});
+$("#content2").mouseup(function () { 
+  $("#content2").removeClass("text-white");
+});
+$("#content3").mousedown(function () { 
+  $("#content3").addClass("text-white");
+});
+$("#content3").mouseup(function () { 
+  $("#content3").removeClass("text-white");
+});
