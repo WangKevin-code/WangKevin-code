2e0d068274167727cc85ece394e54643b19cbaf5
Kevin <kevin22413180@gmail.com>
2022-07-30 10:05:49 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,21 +1,27 @@
 $("#Resume").click(function () {
     window.location.href = './Resume.html';
 });
+
 $("#DataTransfer").click(function () {
     window.location.href = 'https://github.com/WangKevin-code/SQLBroker';
 });
+
 $("#Dashboard").click(function () {
     window.location.href = './Dashboard.html';
 });
+
 $("#WeatherBaord").click(function () {
     window.location.href = './InformationBoard.html';
 });
+
 $("#Form").click(function () {
     window.location.href = './Form.html';
 });
+
 $("#Calculator").click(function () {
     window.location.href = './Vue_calculator.html';
 });
+
 $("#TodoList").click(function () {
     window.location.href = './Vue_todo_list.html';
 });
